846a4a3416f19bc1c947529132135f6be32c7110
Ramon Pacheco <rmpacheco2@gmail.com>
2022-11-06 00:05:19 -0300

    add: shared

---
+++ src/modules/@shared/domain/product-adm/usecase/check-stock/check-stock.usecase.spec.ts
@@ -0,0 +1,34 @@
+import Product from "../../../../../product-adm/domain/product.entity";
+import CheckStockUseCase from "../../../../../product-adm/usecase/check-stock/check-stock.usecase";
+import Id from "../../../value-object/id.value-object";
+
+const product = new Product({
+    id: new Id("1"),
+    name: "Product",
+    description: "Product description",
+    purchasePrice: 100,
+    stock: 10,
+});
+
+const MockRepository = () => {
+    return {
+        add: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    };
+};
+
+describe("CheckStock usecase unit test", () => {
+    it("should get stock of a product", async () => {
+        const ProductRepository = MockRepository();
+        const checkStockUseCase = new CheckStockUseCase(ProductRepository);
+        const input = {
+            productId: "1",
+        };
+
+        const result = await checkStockUseCase.execute(input);
+
+        expect(ProductRepository.find).toHaveBeenCalled();
+        expect(result.productId).toBe("1");
+        expect(result.stock).toBe(10);
+    });
+});
